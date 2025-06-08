ebda569e28d436ca6c55786f452442b780c3650e
Deewansinghrajputs <deeprajput392@gmail.com>
2025-06-08 21:29:29 +0530

    Ready for Vercel deployment

--- src/app/providers/ReduxProvider.tsx
+++ src/app/providers/ReduxProvider.tsx
@@ -1,9 +1,13 @@
 'use client';
-
+import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '@/redux/store';
 
-export default function ReduxProvider({ children }) {
+interface ReduxProviderProps {
+  children: ReactNode;
+}
+
+export default function ReduxProvider({ children }: ReduxProviderProps) {
   return (
     <Provider store={store}>
       {children}
